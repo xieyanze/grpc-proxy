e12e2b945066dd00f778824b5a7ea8165acc0bf0
xieyz <xieyanze@ppmoney.com>
2016-10-24 12:09:42 +0800

    delete code

--- src/main/java/com/andy/grpc/proxy/DecodeHandler.java
+++ src/main/java/com/andy/grpc/proxy/DecodeHandler.java
@@ -1,50 +1,30 @@
 package com.andy.grpc.proxy;
 
-import java.util.ArrayList;
-
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufUtil;
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http2.DefaultHttp2Connection;
-import io.netty.handler.codec.http2.DefaultHttp2ConnectionDecoder;
-import io.netty.handler.codec.http2.DefaultHttp2ConnectionEncoder;
 import io.netty.handler.codec.http2.DefaultHttp2FrameReader;
-import io.netty.handler.codec.http2.DefaultHttp2FrameWriter;
-import io.netty.handler.codec.http2.Http2Connection;
-import io.netty.handler.codec.http2.Http2ConnectionDecoder;
-import io.netty.handler.codec.http2.Http2ConnectionEncoder;
 import io.netty.handler.codec.http2.Http2Exception;
 import io.netty.handler.codec.http2.Http2FrameAdapter;
 import io.netty.handler.codec.http2.Http2FrameLogger;
 import io.netty.handler.codec.http2.Http2FrameReader;
-import io.netty.handler.codec.http2.Http2FrameWriter;
 import io.netty.handler.codec.http2.Http2Headers;
 import io.netty.handler.codec.http2.Http2InboundFrameLogger;
-import io.netty.handler.codec.http2.Http2OutboundFrameLogger;
 
-import static io.netty.buffer.ByteBufUtil.hexDump;
 import static io.netty.handler.logging.LogLevel.INFO;
-import static java.lang.Math.min;
 
 /**
  * DecodeHandler Created by xieyz on 16-10-20.
  */
 @ChannelHandler.Sharable
-public class DecodeHandler extends ChannelDuplexHandler {
+class DecodeHandler extends ChannelDuplexHandler {
 
-    private static String headers = ":authority: 127.0.0.1:4000, :path: /Proxy.ProxyTestService/Echo, :method: POST, " +
-            ":scheme: http, content-type: application/grpc, te: trailers, user-agent: grpc-java-netty/1.0.1, grpc-accept-encoding: gzip";
-
-    public static ThreadLocal<ByteBuf> threadLocal = new ThreadLocal<ByteBuf>();
-
-    private static final Http2FrameLogger HTTP2_FRAME_LOGGER = new Http2FrameLogger(INFO, Http2FrameCodec.class);
+    private static final Http2FrameLogger HTTP2_FRAME_LOGGER = new Http2FrameLogger(INFO, DecodeHandler.class);
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf byteBuf = (ByteBuf) msg;
-        threadLocal.set(byteBuf);
         ByteBuf copy = byteBuf.copy();
         if (byteBuf.getByte(3) == 1) {
             Http2FrameReader reader = new Http2InboundFrameLogger(new DefaultHttp2FrameReader(),
